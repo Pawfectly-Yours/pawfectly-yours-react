04b032f066c91da96745dff65972bd5b9cea6451
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-24 17:32:02 +0800

    add axios instance

---
+++ src/services/axios-instance.ts
@@ -0,0 +1,41 @@
+import { API_KEY, API_URL, DEV_MODE } from "@/config";
+import axios, { AxiosInstance } from "axios";
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: API_URL,
+  timeout: 30000,
+  headers: {
+    "x-api-secret": API_KEY,
+    "x-Frame-Options": "DENY",
+    "Content-Security-Policy": "frame-ancestors 'self'",
+  },
+});
+
+axiosInstance.interceptors.request.use((request) => {
+  // TODO:: for bearer token implementation
+  //   if (localStorage.root) {
+  //     const authStore = JSON.parse(localStorage.root).authStore;
+  //     const token = authStore.accessToken;
+  //     const isLogin: boolean = false;
+  //     if (token && isLogin) {
+  //       request.headers["Authorization"] = `Bearer ${token}`;
+  //     }
+  //   }
+
+  // Microservice devmode reroute
+  if (DEV_MODE === "development") {
+    if (request.url?.includes("auth")) {
+      request.baseURL = "http://localhost:5001/api";
+    } else if (request.url?.includes("admin")) {
+      request.baseURL = "http://localhost:5002/api/";
+    } else if (request.url?.includes("vet-management")) {
+      request.baseURL = "http://localhost:5003/api/";
+    } else if (request.url?.includes("vet-tracker")) {
+      request.baseURL = "http://localhost:5004/api/";
+    } else if (request.url?.includes("pet-adoption")) {
+      request.baseURL = "http://localhost:5004/api/";
+    }
+  }
+
+  return request;
+});
