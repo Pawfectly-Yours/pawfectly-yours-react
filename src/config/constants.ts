45229d9477b5711775fae78e2e146dbbf56376a0
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-26 22:53:13 +0800

    add base url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,2 @@
 export const TEST_VAR: string = "test";
+export const BASE_URL_PREFIX: string = "/react/";
--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,24 +1,25 @@
 import { translate } from "@/i18n";
+import { BASE_URL_PREFIX } from "./constants";
 
 export const PATH = {
   HOME: {
     name: translate("routes.home"),
-    path: "/",
+    path: `${BASE_URL_PREFIX}`,
   },
   LOGIN: {
     name: translate("routes.login"),
-    path: "/login",
+    path: `${BASE_URL_PREFIX}login`,
   },
   REGISTER: {
     name: translate("routes.register"),
-    path: "/register",
+    path: `${BASE_URL_PREFIX}register`,
   },
   DASHBOARD: {
     name: translate("routes.dashboard"),
-    path: "/dashboard",
+    path: `${BASE_URL_PREFIX}dashboard`,
   },
   NOTFOUND: {
     name: translate("routes.notFound"),
-    path: "/not-found",
+    path: `${BASE_URL_PREFIX}not-found`,
   },
 };
